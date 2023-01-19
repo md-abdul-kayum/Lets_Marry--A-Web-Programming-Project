12a89d13f81571e8a40be097e3f306e921c46c1c
Md. Abdul Kayum <97352770+md-abdul-kayum@users.noreply.github.com>
2023-01-20 02:46:14 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,179 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <link rel="canonical" href="https://getbootstrap.com/docs/3.4/examples/jumbotron/">
+
+    <title>Biye Korbo</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+	  <link href="css/style.css" rel="stylesheet">
+	  <link href="css/style1.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+	<!--font-Awesome-->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+  <?php
+  require_once('header.php');
+  ?>
+
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container top-mar">
+	  <div class="center hideform">
+
+
+<div class="banner">
+  <div class="container">
+    <div class="banner_info">
+      <h3>Millions of verified Members</h3>
+      <a href="register.php" class="hvr-shutter-out-horizontal">Create your Profile</a>
+    </div>
+  </div>
+ 
+</div> 
+
+</div>
+        
+        <div class="grid_2">
+	<div class="container">
+		<h2>Wedding Services with Millions of Success Stories</h2>
+       	<div class="heart-divider">
+			<span class="grey-line"></span>
+			<i class="fa fa-heart pink-heart"></i>
+			<i class="fa fa-heart grey-heart"></i>
+			<span class="grey-line"></span>
+        </div>
+        <div class="row_1">
+	     <div class="col-md-8 suceess_story">
+	         <ul> 
+			   <li>
+				  	<div class="suceess_story-date">
+						<span class="entry-1">Dec 20, 2022</span>
+					</div>
+					<div class="suceess_story-content-container">
+						<figure class="suceess_story-content-featured-image">
+						   <img width="75" height="75" src="images/cp.jpg" class="img-responsive" alt=""/>				            
+					    </figure>
+						<div class="suceess_story-content-info">
+				        	<h4><a href="#">Asif & Hiya </a></h4>				        	
+				        	<p>Very good our life<a href="#">More...</a></p>
+				        </div>
+				    </div>
+				</li>
+	            <li>
+				  	<div class="suceess_story-date">
+						<span class="entry-1">Dec 20, 2022</span>
+					</div>
+					<div class="suceess_story-content-container">
+						<figure class="suceess_story-content-featured-image">
+						   <img width="75" height="75" src="images/cp.jpg" class="img-responsive" alt=""/>				            
+					    </figure>
+						<div class="suceess_story-content-info">
+				        	<h4><a href="#">Shezan & ....</a></h4>				        	
+				        	<p>Very good our life<a href="#">More...</a></p>
+				        </div>
+				    </div>
+				</li>
+	            <li>
+				  	<div class="suceess_story-date">
+						<span class="entry-1">Dec 20, 2022</span>
+					</div>
+					<div class="suceess_story-content-container">
+						<figure class="suceess_story-content-featured-image">
+						   <img width="75" height="75" src="images/cp.jpg" class="img-responsive" alt=""/>				            
+					    </figure>
+						<div class="suceess_story-content-info">
+				        	<h4><a href="#">Mahi & ......</a></h4>				        	
+				        	<p>Very good our life<a href="#">More...</a></p>
+				        </div>
+				    </div>
+				</li>
+	            <li>
+				  	<div class="suceess_story-date">
+						<span class="entry-1">Dec 20, 2022</span>
+					</div>
+					<div class="suceess_story-content-container">
+						<figure class="suceess_story-content-featured-image">
+						   <img width="75" height="75" src="images/cp.jpg" class="img-responsive" alt=""/>				            
+					    </figure>
+						<div class="suceess_story-content-info">
+				        	<h4><a href="#">Palash & ....</a></h4>				        	
+				        	<p>Very good our life<a href="#">More...</a></p>
+				        </div>
+				    </div>
+				</li>
+	            <li>
+				  	<div class="suceess_story-date">
+						<span class="entry-1">Dec 20, 2022</span>
+					</div>
+					<div class="suceess_story-content-container">
+						<figure class="suceess_story-content-featured-image">
+						   <img width="75" height="75" src="images/cp.jpg" class="img-responsive" alt=""/>				            
+					    </figure>
+						<div class="suceess_story-content-info">
+				        	<h4><a href="#">He & Her</a></h4>				        	
+				        	<p>Very good our life<a href="#">More...</a></p>
+				        </div>
+				    </div>
+				</li>
+	            <li>
+				  	<div class="suceess_story-date">
+						<span class="entry-1">Dec 20, 2022</span>
+					</div>
+					<div class="suceess_story-content-container">
+						<figure class="suceess_story-content-featured-image">
+						   <img width="75" height="75" src="images/cp.jpg" class="img-responsive" alt=""/>				            
+					    </figure>
+						<div class="suceess_story-content-info">
+				        	<h4><a href="#">He & Her</a></h4>				        	
+				        	<p>Very good our life dfffffffffffffff
+								saddddddddddddddddddddddddddddddddd
+								dtrgsddddddddddddddddddddddddddddddd
+								rtfhsrdfgsefvsdvdrshbdfbdr
+								ydghfbvfdvvvvvvvvsdvddvsvvvvvvvvvagrtsrgsrgsgxfgsrfser<a href="#"></a></p>
+				        </div>
+				    </div>
+				</li>
+	            <li>
+				  	<div class="suceess_story-date">
+						<span class="entry-1">Dec 20, 2022</span>
+					</div>
+					<div class="suceess_story-content-container">
+						<figure class="suceess_story-content-featured-image">
+						   <img width="75" height="75" src="images/cp.jpg" class="img-responsive" alt=""/>				            
+					    </figure>
+						<div class="suceess_story-content-info">
+				        	<h4><a href="#">He & Her</a></h4>				        	
+				        	<p>Very good our life<a href="#">More...</a></p>
+				        </div>
+				    </div>
+				</li>
+	        </ul>
+	    </div>
+		</div>
+	</div>
+      
+      </div>
+    </div>
+
+  <?php
+  require_once('footer.php');
+  ?>
+  </body>
+</html>
